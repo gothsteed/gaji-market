98d9c02048f21ffbec49a6c6e3a33d3ab29a0a8a
allzero25 <2allzero5@gmail.com>
2024-08-26 15:16:35 +0900

    상품목록 controller 연결

---
+++ src/main/java/com/gaji/app/product/controller/productController.java
@@ -0,0 +1,14 @@
+package com.gaji.app.product.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class productController {
+
+    @GetMapping("productList")
+    public String productList() {
+        return "product/productpage";
+    }
+}
