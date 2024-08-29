fb9317754cd27b1b052c9db0386618cf99c8c25b
sssyyyggg <162323298+sssyyyggg@users.noreply.github.com>
2024-08-29 13:30:09 +0900

    회원가입 중간 푸시

---
+++ src/main/java/com/gaji/app/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.gaji.app.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping("memberregister")
+    public String memberregister() {
+        return "member/memberregister";
+    }
+}
