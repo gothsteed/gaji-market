40e781e77b6ec68bd16a1ff5c925285baa6b179b
gothsteed <124808591+gothsteed@users.noreply.github.com>
2024-09-05 15:01:59 +0900

    like product 불러오기

--- src/main/java/com/gaji/app/product/repository/ProductRepository.java
+++ src/main/java/com/gaji/app/product/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.gaji.app.product.repository;
 
+import com.gaji.app.product.domain.Product;
 import com.gaji.app.product.domain.ProductImage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,11 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductImage, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT COUNT(p) FROM Product p WHERE p.member.memberSeq = :memberSeq and p.completestatus='ONSALE'")
     int countOnSaleProductsByMemberSeq(@Param("memberSeq") Long memberSeq);
 
@@ -21,35 +23,6 @@ public interface ProductRepository extends JpaRepository<ProductImage, Long> {
 
     @Query("SELECT COUNT(p) FROM Product p WHERE p.member.memberSeq = :memberSeq and p.completestatus='ONSALE'")
     int countSoldProductsByMemberSeq(@Param("memberSeq") Long memberSeq);
-
-
-    // 상품번호에 대한 상품이미지 가져오기
-    List<ProductImage> findByFkproductseq(Long seq);
-
-    // 상품당 상품이미지 1개씩 가져오기
-    @Query(value = "SELECT * " +
-                   "FROM ( " +
-                   "    SELECT T.*, " +
-                   "           ROW_NUMBER() over (order by " +
-                   "            CASE :sortType " +
-                   "                WHEN 'productseq' THEN T.productseq " +
-                   "                WHEN 'viewcount' THEN T.viewcount " +
-                   "                ELSE T.productseq " +
-                   "            END desc) as RNUM " +
-                   "    FROM ( " +
-                   "        SELECT t1.*, P.* " +
-                   "        FROM tbl_product_image t1 JOIN tbl_product P " +
-                   "        ON t1.fkproductseq = P.productseq " +
-                   "        WHERE productimageseq = ( " +
-                   "            SELECT MIN(t2.productimageseq) " +
-                   "            FROM tbl_product_image t2 " +
-                   "            WHERE t2.fkproductseq = t1.fkproductseq " +
-                   "        ) " +
-                   "    ) T " +
-                   ") " +
-                   "WHERE rnum BETWEEN :start and :end " +
-                   "ORDER BY RNUM ", nativeQuery = true)
-    List<ProductImage> findMinProductImages(@Param("start") int start, @Param("end") int end, @Param("sortType") String sortType);
 
     // 상품 전체 개수 구하기
     @Query(value = "SELECT COUNT(*) FROM tbl_product", nativeQuery = true)
@@ -61,5 +34,7 @@ public interface ProductRepository extends JpaRepository<ProductImage, Long> {
                    "GROUP BY fkproductseq " +
                    "having fkproductseq = :fkproductseq ", nativeQuery = true)
     Long countLikesByProductSeq(Long fkproductseq);
+
+    List<Product> findAllByFkmemberseqAndCompletestatusIn(Long memberSeq, CompleteStatus... completeStatuses);
 }
 
--- src/main/java/com/gaji/app/product/service/ProductService.java
+++ src/main/java/com/gaji/app/product/service/ProductService.java
@@ -1,6 +1,10 @@
 package com.gaji.app.product.service;
 
+import com.gaji.app.product.domain.Product;
 import com.gaji.app.product.domain.ProductImage;
+import com.gaji.app.product.dto.ProductListDto;
+import com.gaji.app.product.repository.CompleteStatus;
+import com.gaji.app.product.repository.ProductImageRepository;
 import com.gaji.app.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,6 +12,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +21,8 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ProductImageRepository productImageRepository;
 
     // 상품 리스트 가져오기
     public Page<ProductImage> getProductList(int pageNumber, String sortType) throws Exception {
@@ -24,11 +31,12 @@ public class ProductService {
         int start = (pageNumber - 1) * pageSize + 1; // 시작 인덱스
         int end = start + pageSize - 1; // 종료 인덱스
 
+
         // 페이징된 결과를 가져옴
         List<ProductImage> pagingProductList = null;
 
         if(!"popular".equals(sortType)) {
-            pagingProductList = productRepository.findMinProductImages(start, end, sortType);
+            pagingProductList = productImageRepository.findMinProductImages(start, end, sortType);
         }
 
         for (ProductImage productImage : pagingProductList) {
@@ -45,19 +53,27 @@ public class ProductService {
         return new PageImpl<>(pagingProductList, PageRequest.of(pageNumber - 1, pageSize), totalCount);
     }
 
-    public List<ProductImage> getUserOnSaleItem(Long userid) {
+    public List<Product> getUserOnSaleItem(Long memberSeq) {
+        List<Product> productList = productRepository.findAllByFkmemberseqAndCompletestatusIn(memberSeq, CompleteStatus.FOR_SALE, CompleteStatus.RESERVED);
 
-        //List<ProductImage> productImageList = productRepository.find(userid);
+        List<ProductListDto> dtos = new ArrayList<>();
+        for(Product product : productList) {
+            ProductListDto dto = new ProductListDto();
+            dto.setImage(product.getFirstImageName());
+            dto.setTitle(product.getTitle());
+            dto.setPrice(product.getPrice());
+        }
+
         return null;
     }
     // 해당 상품 정보 가져오기
     public Optional<ProductImage> getProductById(Long seq) {
-        return productRepository.findById(seq);
+        return productImageRepository.findById(seq);
     }
 
     // 해당 상품 이미지 가져오기
     public List<ProductImage> getProductImgById(Long seq) {
-        return productRepository.findByFkproductseq(seq);
+        return productImageRepository.findByFkproductseq(seq);
     }
 
 }
