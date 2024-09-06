b7aef9fd6c21a59abfee703e43a729d170c0e7b8
sssyyyggg <162323298+sssyyyggg@users.noreply.github.com>
2024-09-06 15:28:08 +0900

    마이페이지 -> 찜한 상품 최종 완료

--- src/main/java/com/gaji/app/member/repository/LikeProductRepository.java
+++ src/main/java/com/gaji/app/member/repository/LikeProductRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LikeProductRepository extends JpaRepository<LikeProduct, Long> {
     
     // 사용자가 좋아요를 표시한 상품 목록을 조회하는 메서드
     List<LikeProduct> findByMember(Member member);
+    
+    Optional<LikeProduct> findByFkmemberseqAndFkproductseq(Long memberseq, Long productseq);
 }
