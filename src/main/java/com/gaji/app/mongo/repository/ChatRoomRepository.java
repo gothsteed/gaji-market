46aa14338fe368d5ddd5cd51728e74491e80a946
YERIN JUNG <156673142+yerin9195@users.noreply.github.com>
2024-09-12 16:04:07 +0900

    yerin ChatController update

--- src/main/java/com/gaji/app/mongo/repository/ChatRoomRepository.java
+++ src/main/java/com/gaji/app/mongo/repository/ChatRoomRepository.java
@@ -1,9 +1,13 @@
 package com.gaji.app.mongo.repository;
 
 import com.gaji.app.mongo.entity.ChatRoom;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
 
+	String findBySellerId(Long sellerMemberSeq);
 
+	String findByBuyerId(Long buyerMemberSeq);
+	
 }
