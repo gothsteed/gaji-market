135243d3c73d73fb12276195bd7be37291e6ca74
KyunghyunKK <2kh94@naver.com>
2024-09-19 09:47:27 +0900

    dto 추가

--- src/main/java/com/gaji/app/mongo/dto/MessageDTO.java
+++ src/main/java/com/gaji/app/mongo/dto/MessageDTO.java
@@ -1,5 +1,6 @@
 package com.gaji.app.mongo.dto;
 
+import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,5 +12,11 @@ public class MessageDTO {
 	private String senderNickname;
 	private String senderType;
 	private String content;
+
+	// JSON 문자열을 MessageDTO 객체로 변환하는 static 메서드
+	public static MessageDTO convertMessage(String source) {
+		Gson gson = new Gson();
+		return gson.fromJson(source, MessageDTO.class);
+	}
 	
 }
